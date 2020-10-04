b6f0ce554e462ed69848c0b5d2c92528f589f74e
Sefran <sefran2992@gmail.com>
2020-10-05 00:55:16 +0200

    Ejercicio form_2 completado

--- tutorialUniweb/util.js
+++ tutorialUniweb/util.js
@@ -1,5 +1,104 @@
 // Util.js: Librería de utilidades para javascript
 
+/* Funcion validarCC
+
+    cogida de internet
+
+*/
+
+function validarIBAN(IBAN) {
+
+    //Se pasa a Mayusculas
+    IBAN = IBAN.toUpperCase();
+    //Se quita los blancos de principio y final.
+    IBAN = IBAN.trim();
+    IBAN = IBAN.replace(/\s/g, ""); //Y se quita los espacios en blanco dentro de la cadena
+
+    var letra1,letra2,num1,num2;
+    var isbanaux;
+    var numeroSustitucion;
+    //La longitud debe ser siempre de 24 caracteres
+    if (IBAN.length != 24) {
+        return false;
+    }
+
+    // Se coge las primeras dos letras y se pasan a números
+    letra1 = IBAN.substring(0, 1);
+    letra2 = IBAN.substring(1, 2);
+    num1 = getnumIBAN(letra1);
+    num2 = getnumIBAN(letra2);
+    //Se sustituye las letras por números.
+    isbanaux = String(num1) + String(num2) + IBAN.substring(2);
+    // Se mueve los 6 primeros caracteres al final de la cadena.
+    isbanaux = isbanaux.substring(6) + isbanaux.substring(0,6);
+
+    //Se calcula el resto, llamando a la función modulo97, definida más abajo
+    resto = modulo97(isbanaux);
+    if (resto == 1){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function modulo97(iban) {
+    var parts = Math.ceil(iban.length/7);
+    var remainer = "";
+
+    for (var i = 1; i <= parts; i++) {
+        remainer = String(parseFloat(remainer+iban.substr((i-1)*7, 7))%97);
+    }
+
+    return remainer;
+}
+
+function getnumIBAN(letra) {
+    ls_letras = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return ls_letras.search(letra) + 10;
+}
+
+/* Función comprobarSiHayItemSeleccionado
+
+Esta función permite saber si hay un item seleccionado en un radio button
+
+ - Entradas: botonRadio (elemento radio button)
+ - Salida: haySeleccionado (bool)
+
+*/
+
+function comprobarSiHayItemSeleccionado(botonRadio){
+
+    var haySeleccionado = false;
+
+    for (var i in  botonRadio){
+        if (botonRadio[i].checked) haySeleccionado = true;
+    }
+
+    return haySeleccionado;
+}
+
+
+/* Función comprobarLongitudTexto
+
+Esta función permite saber si una cadena de caracteres tiene menos de una longitud determinada
+
+ - Entradas: cadena (str), longitud (int)
+ - Salida: valido (bool)
+
+*/
+
+function comprobarLongitudTexto(cadena, longitud){
+
+    valido = true;
+
+    if (typeof cadena != "string" || typeof longitud != "number") valido = false;
+    if (cadena.length < longitud) valido = false;
+
+    return valido;
+
+}
+
+
 /* Función validarFecha
 
 Esta función nos comprobará que una fecha es válida o no en función de su día, mes y año.
@@ -17,8 +116,6 @@ function validarFecha(dia,mes,anio){
     var bisiesto = anio % 4 == 0;
 
     // Comprobamos que los datos del usuario son correctos
-
-    console.log( fechaUsuario.getFullYear() );
 
     if (typeof(dia) != "number" || typeof(mes) != "number" || typeof(anio) != "number") valido = false;
 
@@ -44,7 +141,6 @@ function validarFecha(dia,mes,anio){
                 break;
         }
     }
-
     return valido;
 }
 
@@ -69,6 +165,65 @@ function soloLetras(e){
 
     return valido;
 }		
+
+/* Función soloNumerosDecimales
+
+Esta función permite comprobar si una tecla introducida es un valor numérico entero o un punto
+
+ - Entradas: evento (keyUp)
+ - Salida: valido (booleano)
+
+*/
+
+function soloNumerosDecimales(e){
+	
+    var valido = true;
+
+    if (".0123456789".indexOf(e.key) == -1) valido = false;
+
+    return valido;
+}	
+
+/* Función soloNumeros
+
+Esta función permite comprobar si una tecla introducida es un valor numérico entero
+
+ - Entradas: evento (keyUp)
+ - Salida: valido (booleano)
+
+*/
+
+function soloNumeros(e){
+	
+    var valido = true;
+
+    if ("0123456789".indexOf(e.key) == -1) valido = false;
+
+    return valido;
+}	
+
+/* Función soloAlfanumericos
+
+Esta función permite comprobar si una tecla introducida es un valor numérico o alfabético español
+
+ - Entradas: evento (keyUp)
+ - Salida: valido (booleano)
+
+*/
+
+function soloAlfanumericos(e){
+	
+    var valido = true;
+
+    if ((e.key >= 'a' && e.key <= 'z') ||
+        (e.key >= 'A' && e.key <= 'Z' ) || 
+        "áéíóúÁÉÍÓÚñÑ ".indexOf(e.key) != -1 ||
+        "0123456789".indexOf(e.key) == -1) {
+            valido = true;
+        }
+
+    return valido;
+}	
 
 /* Función validarDni
 
